02cf152b64c96f6a5481acab037b0c47599aaaff
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-05-11 21:52:02 -0300

    tempo de recarregar
    cometei o codigo que altera o tempo pra pagina painel.php recarregar

--- app/views/painel.php
+++ app/views/painel.php
@@ -40,7 +40,7 @@ foreach ($pedidosWorkflow as $pedido) {
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="5"> <!-- Esta linha atualiza a página a cada 50 segundos -->
+    <!--<meta http-equiv="refresh" content="5">  Esta linha atualiza a página a cada 50 segundos -->
     <title>Painel - Flor de Cheiro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
